65b0f827a9aa9c2fdb84901c362c1b3c484b91c9
Tso <62343478+pomeluce@users.noreply.github.com>
2025-11-02 18:09:07 +0800

    feat: updated flake build config

--- flake.nix
+++ flake.nix
@@ -7,38 +7,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, ags }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       rev = self.rev or self.dirtyRev;
 
       nativeBuildInputs = with pkgs; [
-        wrapGAppsHook
+        wrapGAppsHook3
         gobject-introspection
         (ags.packages.${system}.default.override {
           extraPackages = buildInputs;
         })
       ];
 
-      buildInputs = (with ags.packages.${system}; [
-        io
-        astal3
-        astal4
-        apps
-        auth
-        battery
-        bluetooth
-        hyprland
-        mpris
-        network
-        notifd
-        powerprofiles
-        tray
-        wireplumber
-      ]) ++ (with pkgs; [ gjs libadwaita ]);
+      buildInputs =
+        (with ags.packages.${system}; [
+          io
+          astal3
+          astal4
+          apps
+          auth
+          battery
+          bluetooth
+          hyprland
+          mpris
+          network
+          notifd
+          powerprofiles
+          tray
+          wireplumber
+        ])
+        ++ (with pkgs; [
+          gjs
+          libadwaita
+        ]);
 
-      mkPackage = name:
+      mkPackage =
+        name:
         pkgs.stdenvNoCC.mkDerivation {
           inherit nativeBuildInputs buildInputs;
 
@@ -48,43 +59,45 @@
 
           installPhase = ''
             mkdir -p $out/bin
-            ags bundle dist/${name}.ts $out/bin/${name} --define "VERSION='${
-              builtins.substring 0 7 rev
-            }'"
+            ags bundle dist/${name}.ts $out/bin/${name} --define "VERSION='${builtins.substring 0 7 rev}'"
           '';
 
           preFixup = ''
              gappsWrapperArgs+=(
               --prefix PATH : ${
                 with pkgs;
-                lib.makeBinPath (buildInputs ++ [
-                  gtk3
-                  gtk4
-                  dart-sass
-                  fzf
-                  hyprpicker
-                  brightnessctl
-                  wl-clipboard
-                  wf-recorder
-                  wayshot
-                  slurp
-                  swappy
-                  cliphist
-                  pipewire
-                  wireplumber
-                  gnome-control-center
-                  gnome-calendar
-                  gnome-bluetooth
-                  swww
-                  networkmanager
-                  matugen
-                ])
+                lib.makeBinPath (
+                  buildInputs
+                  ++ [
+                    gtk3
+                    gtk4
+                    dart-sass
+                    fzf
+                    hyprpicker
+                    brightnessctl
+                    wl-clipboard
+                    wf-recorder
+                    wayshot
+                    slurp
+                    swappy
+                    cliphist
+                    pipewire
+                    wireplumber
+                    gnome-control-center
+                    gnome-calendar
+                    gnome-bluetooth
+                    swww
+                    networkmanager
+                    matugen
+                  ]
+                )
               }
             )
           '';
         };
 
-    in {
+    in
+    {
 
       packages.${system} = {
         default = self.packages.${system}.akir-shell;
